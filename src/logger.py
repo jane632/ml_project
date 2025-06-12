cb011168ced109b14f025b78557970c60e159514
Jane Njuguna <janenjuguna922@gmail.com>
2025-06-12 18:52:46 +0300

    EDA And Probrlm Statement

--- src/logger.py
+++ src/logger.py
@@ -2,6 +2,7 @@ import logging
 import os
 from datetime import datetime
 
+
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs" , LOG_FILE)
 os.makedirs(logs_path,exist_ok = True)
@@ -16,5 +17,27 @@ logging.basicConfig(
 )
 
 
+
+
+import sys
+import logging
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+    )
+    return error_message
+
+
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail = error_detail)
+
+    def __str__(self):
+        return self.error_message    
 
 
